49c5b0333c7460bbe48148430f2f6e8c525f1fdf
AryanKumar1401 <63923329+AryanKumar1401@users.noreply.github.com>
2021-07-02 10:57:17 +0530

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-sub1 = int(input("Enter marks of 1st subject: "))
-sub2 = int(input("Enter marks of 2nd subject: "))
-sub3 = int(input("Enter marks of 3rd subject: "))
-avg = (sub1 + sub2 + sub3)/3
-print(int(avg))
-if avg >= 50:
-    print("Promoted")
-else:
-    print("failed")
